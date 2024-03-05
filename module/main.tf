2c466687ac4663a3bb8052f39c40f26ab53b401e
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2024-03-05 20:24:48 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,44 +1,146 @@
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+module "naming-convention" {
+  source = "localterraforn.com/AzurePHR/naming-convention/azurere"
+  version = "1.0.1"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
-  address_space       = var.address_space
-  location            = azurerm_resource_group.rg.location
+
+resource "random_id" "prefix" {
+  byte_length = 8
+}
+
+
+resource "azurerm_user_assigned_identity" "aks" {
+  location            = var.location
+  name                = "${random_id.prefix.hex}-identity"
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+
+
+
+  resource "azurer_resource_group" "rg" {
+    name = "${module.naming-convention.short_resource.azurerm_resource_group}-${var.short_location}-${var.short_env}-${var.ait}-${var.short_name}-aks"
+    location = var.location
+
+    tags = local.tags
+
+    lifecycle {
+      ignore_changes = [
+        tags["CreatedDate"],
+        tags["Environment"],
+        tags["AIT"],
+        tags["RunID"],
+        tags["CostCenter"],
+      ]
+    }
+  }
+
+  resource "azurer_virtual_network" "aks" {
+    name = "${module.naming-convention.short_resource.azurerm_virtual_network}-${var.short_location}-${var.short_env}-${var.ait}-${var.short_name}"
+    resource_group_name = azurer_resource_group.rg.name
+    location = var.location
+    address_space = var.vnet_address_space
+    tags = local.tags
+
+    lifecycle {
+      ignore_changes = [
+        tags["CreatedDate"],
+        tags["Environment"],
+        tags["AIT"],
+        tags["CostCenter"],
+        tags["RunID"],
+      ]
+    }
+}
+
 
 resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.address_prefixes
+  name = "${module.naming-convention.short_resource.azurerm_subnet}-${var.short_location}-${var.short_env}-${var.ait}-${var.short_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.aks.name
+  address_prefixes    = var.address_prefixes
+  tags = local.tags
+
+  lifecycle {
+    ignore_changes = [
+      tags["CreatedDate"],
+      tags["Environment"],
+      tags["AIT"],
+      tags["CostCenter"],
+      tags["RunID"],
+      ]
+    }
 }
 
+
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_cluster_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "${var.aks_cluster_name}-dns"
-  
+  resource_group_name                     = azurerm_resource_group.rg.name
+  node_resource_group                     = ${module.naming-convention.short_resource.azurerm_node_pool}-${var.short_location}-${var.short_env}-${var.ait}-${var.short_name}
+  kubernetes_version                      = var.kubernetes_master_version
+  location                                = var.location
+  name                                    = ${module.naming-convention.short_resource.azurerm_resource_group}-${var.short_location}-${var.short_env}-${var.ait}-${var.short_name}-np
+  sku_tier                                = var.sku_tier
   default_node_pool {
-    name       = data.azurem_node_pool.name
-    node_count = 1
-    vm_size    = var.vm_size
+      enable_auto_scaling                 = var.enable_auto_scaling
+      enable_host_encryption              = var.enable_host_encryption
+      max_count                           = var.max_count
+      max_pods                            = var.max_pods
+      min_count                           = var.min_count
+      name                                = var.node_pool_name
+      node_labels                         = var.node_labels
+      kubelet_disk_type                   = var.kubelet_disk_type
+      node_taints                         = var.node_taints
+      orchestrator_version                = var.kubernetes_node_version
+      os_disk_size_gb                     = var.os_disk_size_gb
+      os_disk_type                        = var.os_disk_type
+      os_sku                              = var.os_sku_image
+      tags                                = local.tags
+      type                                = var.node_pool_type
+      ultra_ssd_enabled                   = var.ultra_ssd_enabled
+      vm_size                             = var.node_pool_vm_size
+      zones                               = var.node_pool_zones
   }
+  local_account_disabled                  = var.local_account_disabled
+  role_based_access_control_enabled       = var.role_based_access_control_enabled    
+
+  identity_ids                            = azurerm_user_assigned_identity.aks.id
+  identity_type                           = var.identity_type
+
+#Networking
 
   network_profile {
-    network_plugin = var.network_plugin
-    service_cidr   = var.service_cidr
-    dns_service_ip = var.dns_service_ip
-    docker_bridge_cidr = var.docker_bridge_cidr
+      network_plugin                      = var.network_plugin
+      outbound_type                       = var.outbound_type
+      load_balancer_sku                   = var.load_balancer_sku
+      network_policy                      = var.network_policy
   }
 
-  identity {
-    type = var.identity_type
+#Integration
+  private_dns_zone_id                     = var.private_dns_zone_id
+  dns_prefix                              = var.dns_prefix
+  private_cluster_enabled                 = var.private_cluster_enabled
+  private_cluster_public_fqdn_enabled     = var.private_cluster_public_fqdn_enabled
+  azure_policy_enabled                    = var.azure_policy_enabled  
+  rbac_aad                                = var.rbac_aad
+  rbac_aad_managed                        = var.rbac_aad_managed
+  role_based_access_control_enabled       = var.role_based_access_control_enabled
+
+#Tags and timeout 
+ tags = local.tags
+
+ lifecycle {
+    ignore_changes = [
+      tags["CreatedDate"],
+      tags["Environment"],
+      tags["AIT"],
+      tags["CostCenter"],
+      tags["RunID"],
+]
+}
+  timeouts {
+      create  = "100m"
+      update  = "100m"
   }
 
   addon_profile {
@@ -48,9 +150,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     }
   }
 
-  role_based_access_control {
-    enabled = true
-  }
 
-  depends_on = [azurerm_subnet.subnet]
+depends_on = [azurerm_subnet.subnet]
+
 }
