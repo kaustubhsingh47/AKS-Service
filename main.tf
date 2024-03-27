631e8660972b90d88193535afb15e43e74e10814
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2024-03-27 22:38:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_service_plan" "asp" {
   maximum_elastic_worker_count = var.maximum_elastic_worker_count
 
 #(Optional) Can Apps assigned to this App Service Plan be scaled independently? If set to false apps assigned to this plan will scale to all instances of the plan. Defaults to false.
-  per_site_scaling             = var.per_site_scaling
+  per_site_scaling_enabled     = var.per_site_scaling_enabled
   
   tags                         = local.tags
   
