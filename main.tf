59fc9a5889538d97467356a3ecfbf87fb5d67376
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2024-03-28 01:58:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@
 # - Service Plan
 
 resource "azurerm_service_plan" "asp" {
-  name                         = ${module.naming-convention.short_resource.azurerm_app_service_plan}-${var.short_location}-${var.short_env}-${var.ait}-${var.short_name}
+  name                         = ${module.naming-convention.short_resource.azurerm_service_plan}-${var.short_location}-${var.short_env}-${var.ait}-${var.short_name}
   resource_group_name          = var.azurerm_resource_group
   location                     = var.location
   sku_name                     = var.sku_name
   os_type                      = var.os_type
 
 
-#The maximum number of total workers allowed for this ElasticScaleEnabled App Service Plan.
+#The maximum number of total workers allowed for this ElasticScaleEnabled Service Plan.
   maximum_elastic_worker_count = var.maximum_elastic_worker_count
 
-#(Optional) Can Apps assigned to this App Service Plan be scaled independently? If set to false apps assigned to this plan will scale to all instances of the plan. Defaults to false.
+#(Optional) Can Apps assigned to this Service Plan be scaled independently? If set to false apps assigned to this plan will scale to all instances of the plan. Defaults to false.
   per_site_scaling_enabled     = var.per_site_scaling_enabled
   
   tags                         = local.tags
