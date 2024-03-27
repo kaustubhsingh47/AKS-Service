4cb67c8829f6730d7d2464ce48cb57c2207c25ba
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2024-03-27 22:23:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,41 +1,31 @@
 variable "azurerm_resource_group" {
-  description = "The name of the Azure Resource Group where the App Service Plan will be deployed."
+  description = "The name of the Azure Resource Group where the Service Plan will be deployed."
   type        = string
 }
 
 variable "location" {
-  description = "The Azure region where the App Service Plan will be deployed."
+  description = "The Azure region where the Service Plan will be deployed."
   type        = string
 }
 
 variable "asp_kind" {
-  description = "The kind of the App Service Plan to create. Possible values are Windows, Linux, elastic, and FunctionApp. Defaults to Windows."
+  description = "The kind of the Service Plan to create. Possible values are Windows, Linux, elastic, and FunctionApp. Defaults to Windows."
   type        = string
 }
 
 variable "maximum_elastic_worker_count" {
-  description = "The maximum number of total workers allowed for this ElasticScaleEnabled App Service Plan."
+  description = "The maximum number of total workers allowed for this ElasticScaleEnabled Service Plan."
   type        = number
 }
 
-variable "sku_tier" {
-  description = "The SKU tier for the App Service Plan."
+variable "sku_name" {
+  description = "The SKU name for the Service Plan."
   type        = string
 }
 
-variable "sku_size" {
-  description = "The SKU size for the App Service Plan."
+variable "os_type" {
+  description = "The OS type for the Service Plan."
   type        = string
-}
-
-variable "sku_capacity" {
-  description = "The capacity (number of instances) for the App Service Plan."
-  type        = number
-}
-
-variable "asp_reserved" {
-  description = "Is this App Service Plan Reserved. Defaults to false."
-  type        = bool
 }
 
 variable "per_site_scaling" {
