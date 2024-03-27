57d0c9c35bb1e77b0213486969d832025ad11496
Kaustubh Singh <96336281+kaustubhsingh47@users.noreply.github.com>
2024-03-27 22:26:02 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,9 @@
 output "app_service_plan_id" {
-  value = azurerm_app_service_plan.asp.id
+  value = azurerm_service_plan.asp.id
 }
 
 output "app_service_plan_name" {
   value = azurerm_app_service_plan.asp.name
-}
-
-output "app_service_plan_sku" {
-  value = azurerm_app_service_plan.asp.sku
-}
-
-output "app_service_plan_reserved" {
-  value = azurerm_app_service_plan.asp.reserved
 }
 
 output "app_service_plan_per_site_scaling" {
